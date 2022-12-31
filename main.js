8d971bd399eb9b80a4e416491c6dbe08e034c87c
Anthony <autosonic7@hotmail.com>
2022-12-31 15:17:31 -0800

    Added date to weather app

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function setQuery(evt) {
 }
 
 function getResults (query) {
-    fetch(`${api.base}weather?q=${{query}&units=metric&APPID=${api.key}`)
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
     .then(weather => {
         return weather.json();
     }).then(displayResults);
@@ -25,5 +25,23 @@ function getResults (query) {
 function displayResults (weather) {
     console.log(weather);
     let city = document.querySelector('.location .city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    let now = new Date();
+    let date = document.querySelector('.location.date');
+    date.innerText = dateBuilder(now);
 }
     
+function dateBuilder (d) {
+    let months = ["January", "February", "March", "April", "May", "June",
+"July", "August", "September", "October", "November", "December"];
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
+"Friday", "Saturday"];
+
+let day =days[d.getDay()];
+let date = d.getDate();
+let month = months[d.getMonths()];
+let year = d.getFullYear();
+
+return `${day} ${date} ${month} ${year}`;
+}
