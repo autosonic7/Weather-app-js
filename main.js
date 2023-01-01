3ffcebdd65a523264bdc126a44f183955da2ec7a
Anthony <autosonic7@hotmail.com>
2022-12-31 16:27:33 -0800

    Weather app working

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const api = {
     key: "43404d29aa1c79d3737ff0a16883f5c2",
-    baseurl:"https://home.openweathermap.org/data/2.5/",
+    base:"http://api.openweathermap.org/data/2.5/"
+
 
 }
 
@@ -28,8 +29,17 @@ function displayResults (weather) {
     city.innerText = `${weather.name}, ${weather.sys.country}`;
 
     let now = new Date();
-    let date = document.querySelector('.location.date');
+    let date = document.querySelector('.location .date');
     date.innerText = dateBuilder(now);
+
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+
+    let weather_el = document.querySelector('.current .weather');
+    weather_el.innerText = weather.weather[0].main;
+
+    let hilow = document.querySelector('.hi-low');
+    hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
 }
     
 function dateBuilder (d) {
@@ -40,7 +50,7 @@ let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
 
 let day =days[d.getDay()];
 let date = d.getDate();
-let month = months[d.getMonths()];
+let month = months[d.getMonth()];
 let year = d.getFullYear();
 
 return `${day} ${date} ${month} ${year}`;
